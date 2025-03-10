3383345aab5b6f340879c1d0f359b6cbecaaf772
Djordje Jokic <djordje.jokic@gmail.com>
2025-03-10 10:54:38 +0100

    More factory and strategy examples.

---
+++ src/main/java/org/example/strategy/guru2/Strategy.java
@@ -0,0 +1,5 @@
+package org.example.strategy.guru2;
+
+public interface Strategy {
+    int execute(int a, int b);
+}
---
+++ src/main/java/org/example/strategy/guru2/StrategySubtract.java
@@ -0,0 +1,8 @@
+package org.example.strategy.guru2;
+
+public class StrategySubtract implements Strategy{
+    @Override
+    public int execute(int a, int b) {
+        return a - b;
+    }
+}
